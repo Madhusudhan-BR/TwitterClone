dc4c7e90a784f53013a0935648ff42f444624223
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-07-26 10:20:58 +0530

    connected outlets

--- TwitterClone/RegisterController.swift
+++ TwitterClone/RegisterController.swift
@@ -1,25 +1,36 @@
 //
-//  ViewController.swift
+//  RegisterController.swift
 //  TwitterClone
 //
-//  Created by Madhusudhan B.R on 7/25/17.
+//  Created by Madhusudhan B.R on 7/26/17.
 //  Copyright © 2017 Madhusudhan. All rights reserved.
 //
 
 import UIKit
 
 class RegisterController: UIViewController {
-
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var lastnameField: UITextField!
+    
+    @IBOutlet weak var firstnameField: UITextField!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad() 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func handleRegister(_ sender: Any) {
     }
-
-
+    
+    @IBAction func handleAlreadyHaveAccount(_ sender: Any) {
+    }
+    
+    
 }
-
