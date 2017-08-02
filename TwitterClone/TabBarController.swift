3dd4dfcab69b85c1908d3021fbc860f5012521b9
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-08-02 17:40:39 +0530

    customised tab bar

---
+++ TwitterClone/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  TwitterClone
+//
+//  Created by Madhusudhan B.R on 8/2/17.
+//  Copyright © 2017 Madhusudhan. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .white
+        
+    }
+}
