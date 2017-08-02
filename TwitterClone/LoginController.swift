af317156d1e0792cd6a250ab86d27c6b1a4cd1a8
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-08-02 12:39:46 +0530

    logged in user

--- TwitterClone/LoginController.swift
+++ TwitterClone/LoginController.swift
@@ -25,7 +25,43 @@ class LoginController: UIViewController {
             usernameField.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSForegroundColorAttributeName : UIColor.red])
             passwordField.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSForegroundColorAttributeName : UIColor.red])
         }
-        
+        else {
+            guard let username = usernameField.text?.lowercased(), let password = passwordField.text else { return }
+            
+            guard  let url = URL(string: "http://localhost/twitterclone/Login.php") else {
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            let body = "username=\(username)&password=\(password)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let data = data else { return }
+                
+                DispatchQueue.main.async {
+                    do {
+                        guard let jsonData =  try  JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary else { return }
+                        
+                        print(jsonData)
+                        
+                    } catch let jsonError {
+                        print(jsonError.localizedDescription)
+                        return
+                    }
+                   
+                    
+                }
+                
+            }).resume()
+            
+        }
         
     }
 }
