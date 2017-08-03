418322a991b3554c5642dec6fc57fb2ff888e957
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-08-03 11:08:27 +0530

    customised tab bar image color

--- TwitterClone/TabBarController.swift
+++ TwitterClone/TabBarController.swift
@@ -14,6 +14,34 @@ class TabBarController: UITabBarController {
         
         tabBar.tintColor = .white
         tabBar.barTintColor = UIColor(red: 45/255, green: 213/255, blue: 255/255, alpha: 1)
-        tabBar.isTranslucent = false 
+        tabBar.isTranslucent = false
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.black], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.white], for: .selected)
+        
+        for item in self.tabBar.items as! [UITabBarItem]{
+            if  let image = item.image {
+                item.image = image.imageColor(color: UIColor.lightGray).withRenderingMode(.alwaysOriginal)
+            }
+        }
+    }
+}
+
+extension UIImage {
+    func imageColor(color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let context = UIGraphicsGetCurrentContext() as! CGContext
+        context.translateBy(x: 0, y: self.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        context.clip(to: rect, mask: self.cgImage!)
+        
+        color.setFill()
+        context.fill(rect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as! UIImage
+        UIGraphicsEndImageContext()
+        return newImage
     }
 }
